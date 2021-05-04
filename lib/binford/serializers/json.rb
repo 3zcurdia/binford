ce70d2b3748f4f42aaf8bcd78e49a2a918c63ed9
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2021-05-04 07:42:09 -0500

    Fix json serializer

--- lib/binford/serializers/json.rb
+++ lib/binford/serializers/json.rb
@@ -5,8 +5,8 @@ require "json"
 module Binford
   module Serializers
     class Json
-      def call(response)
-        JSON.parse(response.body)
+      def call(content)
+        JSON.parse(content, symbolize_names: true)
       rescue JSON::ParserError
         nil
       end
